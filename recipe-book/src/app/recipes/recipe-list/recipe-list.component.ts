169fde44440589324a60a13c27f78457063281c5
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2016-10-22 10:44:18 +0100

    Add first-app and recipe-book course projects

---
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../recipe';
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent {
+  recipes:Recipe[] = [
+    new Recipe('Schnitzel', 'What is this?', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', []),
+    new Recipe('Summer Salad', 'Yummy', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', [])
+  ];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+}
