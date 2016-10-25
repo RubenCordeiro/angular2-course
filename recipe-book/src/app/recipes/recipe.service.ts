9965766a54f2d72304578c0fd6ff2f289c9b9e88
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2016-10-25 12:12:04 +0100

    feat: add RecipeService

---
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient';
+import { Recipe } from './recipe';
+
+@Injectable()
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'What is this?', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1)
+    ]),
+    new Recipe('Summer Salad', 'Yummy', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', [])
+  ];
+
+  constructor() { }
+
+  getRecipes() {
+    return this.recipes;
+  }
+}
